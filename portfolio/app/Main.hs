62d79c10539f0e2efd0ab67afadd791eeb70c7ab
TsukiGva2 <tsukigva@gmail.com>
2024-07-02 09:36:01 -0300

    Haskell markdowner!
    Github actions time!

--- portfolio/app/Main.hs
+++ portfolio/app/Main.hs
@@ -3,7 +3,26 @@
 module Main where
 
 import CMark
-import Data.Text ()
 
-main :: IO()
-main = putStrLn . decodeUtf8 . encode $ commonmarkToHtml [optSmart] "Hello, Haskell!"
+import System.Environment
+import System.Exit
+
+import qualified Data.Text as T
+import qualified Data.Text.IO as TIO
+
+toHTML :: T.Text -> T.Text
+toHTML str = commonmarkToHtml [optSmart] str
+
+parse :: [String] -> IO T.Text
+parse [] = usage >> fatal >> return T.empty
+parse fs = T.concat <$> mapM TIO.readFile fs
+
+fatal :: IO ()
+fatal = exitWith (ExitFailure 1)
+
+usage :: IO ()
+usage = TIO.putStrLn "Usage: portfolio [-h] [file ..]"
+
+main :: IO ()
+main = getArgs >>= parse >>= TIO.writeFile "out.html" . toHTML
+
